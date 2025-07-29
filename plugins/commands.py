f59df7f12c2de0d747062a57bdd94e54903ba26d
Sellotha <laptop7033@gmail.com>
2025-07-30 02:44:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -55,7 +55,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton(text="💗", callback_data="start"),
+                    InlineKeyboardButton(text="⚔️", callback_data="start"),
                     InlineKeyboardButton(text="🛡", callback_data="group_info"),
                     InlineKeyboardButton(text="💮", callback_data="about"),
                     InlineKeyboardButton(text="🌀", callback_data="shortlink_info"),
