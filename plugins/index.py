c0e6240bc80c42b4c5d0eca6bd96b1d7ed66394b
Sellotha <laptop7033@gmail.com>
2025-07-30 04:43:56 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -13,8 +13,11 @@ logger.setLevel(logging.INFO)
 lock = asyncio.Lock()
 
 
-@Client.on_callback_query(filters.regex(r'^index'))
+@Client.on_callback_query()
 async def index_files(bot, query):
+    if not query.data.startswith("index#"):
+        return
+        
     if query.data.startswith('index_cancel'):
         temp.CANCEL = True
         return await query.answer("Cancelling Indexing")
