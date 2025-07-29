4a172ece3b740e6b120822a0c6f59e52e140ee0e
Sellotha <laptop7033@gmail.com>
2025-07-30 04:51:25 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -16,41 +16,47 @@ lock = asyncio.Lock()
 @Client.on_callback_query()
 async def index_files(bot, query):
     if not query.data.startswith("index#"):
-        return
-        
+        return  # Ignore other callbacks
+
     if query.data.startswith('index_cancel'):
         temp.CANCEL = True
         return await query.answer("Cancelling Indexing")
+
     _, raju, chat, lst_msg_id, from_user = query.data.split("#")
+
     if raju == 'reject':
         await query.message.delete()
         await bot.send_message(int(from_user),
-                               f'Your Submission for indexing {chat} has been declined by our moderators.',
-                               reply_to_message_id=int(lst_msg_id))
+            f'Your Submission for indexing {chat} has been declined by our moderators.',
+            reply_to_message_id=int(lst_msg_id))
         return
 
     if lock.locked():
         return await query.answer('Wait until previous process complete.', show_alert=True)
-    msg = query.message
 
     await query.answer('Processing...💗', show_alert=True)
+
+    msg = query.message
+
     if int(from_user) not in ADMINS:
         await bot.send_message(int(from_user),
-                               f'Your Submission for indexing {chat} has been accepted by our moderators and will be added soon.',
-                               reply_to_message_id=int(lst_msg_id))
-    await msg.edit(
+            f'Your Submission for indexing {chat} has been accepted by our moderators and will be added soon.',
+            reply_to_message_id=int(lst_msg_id))
+
+    await msg.edit_text(
         "Starting Indexing",
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton('Cancel', callback_data='index_cancel')]]
         )
     )
+
     try:
         chat = int(chat)
     except:
-        chat = chat
+        pass
+
     await index_files_to_db(int(lst_msg_id), chat, msg, bot)
-
-
+    
 @Client.on_message((filters.forwarded | (filters.regex(r"(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text ) & filters.private & filters.incoming)
 async def send_for_index(bot, message):
     if message.text:
