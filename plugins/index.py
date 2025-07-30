0e7a0ac5007df84402fd56ab41addc4268ed9c61
Sellotha <laptop7033@gmail.com>
2025-07-30 11:50:47 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -11,57 +11,6 @@ import re
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 lock = asyncio.Lock()
-
-
-@Client.on_callback_query()
-async def index_files(bot, query):
-    data = query.data
-
-    if data == 'index_cancel':
-        temp.CANCEL = True
-        return await query.answer("Cancelling Indexing")
-
-    if not data.startswith("index#"):
-        return  # Ignore unrelated callbacks
-
-    parts = data.split("#")
-    if len(parts) < 5:
-        return await query.answer("Invalid callback data.", show_alert=True)
-
-    _, raju, chat, lst_msg_id, from_user = parts
-    
-    if raju == 'reject':
-        await query.message.delete()
-        await bot.send_message(int(from_user),
-            f'Your Submission for indexing {chat} has been declined by our moderators.',
-            reply_to_message_id=int(lst_msg_id))
-        return
-
-    if lock.locked():
-        return await query.answer('Wait until previous process complete.', show_alert=True)
-
-    await query.answer('Processing...💗', show_alert=True)
-
-    msg = query.message
-
-    if int(from_user) not in ADMINS:
-        await bot.send_message(int(from_user),
-            f'Your Submission for indexing {chat} has been accepted by our moderators and will be added soon.',
-            reply_to_message_id=int(lst_msg_id))
-
-    await msg.edit_text(
-        "Starting Indexing",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton('Cancel', callback_data='index_cancel')]]
-        )
-    )
-
-    try:
-        chat = int(chat)
-    except:
-        pass
-
-    await index_files_to_db(int(lst_msg_id), chat, msg, bot)
     
 @Client.on_message((filters.forwarded | (filters.regex(r"(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text ) & filters.private & filters.incoming)
 async def send_for_index(bot, message):
