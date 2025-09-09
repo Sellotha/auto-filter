e3b7bdd0b57786876d2b597ef81974e457499315
Sellotha <laptop7033@gmail.com>
2025-09-10 03:17:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -274,7 +274,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = "<b>\"{file_name}\"</b>\n'⚙️ 𝚂𝚒𝚣𝚎 ~ <b>{file_size}</b>\n⚜️ 𝙿𝚘𝚜𝚝 𝚋𝚢 ~ 𝐌𝐎𝐕𝐈𝐄 𝐓𝐀𝐋𝐊'\n\n'⚡ 𝐉𝐨𝐢𝐧 𝐔𝐬 ~ ❤️\n➥『 @Movie_Talk_Backup 』'"
+    CAPTION = "> <b>{file_name}</b>\n\n> ⚙️ 𝚂𝚒𝚣𝚎 ~ <b>[{file_size}]</b>\n> ⚜️ 𝙿𝚘𝚜𝚝 𝚋𝚢 ~ <b>𝐌𝐎𝐕𝐈𝐄 𝐓𝐀𝐋𝐊</b>\n\n> ⚡ 𝐉𝐨𝐢𝐧 𝐔𝐬 ~ ❤️\n> ➥『 @Movie_Talk_Backup 』"
 
     IMDB_TEMPLATE_TXT = """
 <b>‣ ᴛɪᴛʟᴇ : <a href={url}>{title}</a>
