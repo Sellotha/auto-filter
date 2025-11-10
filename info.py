32129a3d661e24498d089ee420818cffe6b51333
Sellotha <laptop7033@gmail.com>
2025-11-10 21:27:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -77,6 +77,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'files')
 # ============================
 MOVIE_UPDATE_NOTIFICATION = bool(environ.get('MOVIE_UPDATE_NOTIFICATION', False))  # Notification On (True) / Off (False)
 IMAGE_FETCH = bool(environ.get('IMAGE_FETCH', True))  # On (True) / Off (False)
+TMDB_API_KEY = environ.get('TMDB_API_KEY', '')
 CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla", "Telugu", "Malayalam", "Kannada", "Marathi", "Punjabi", "Bengoli", "Gujrati", "Korean", "Gujarati", "Spanish", "French", "German", "Chinese", "Arabic", "Portuguese", "Russian", "Japanese", "Odia", "Assamese", "Urdu"]
 
 # ============================
