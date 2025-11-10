ed133280d2b8fc9dedcf8d34cdce2b6ab75eb1b6
Sellotha <laptop7033@gmail.com>
2025-11-10 19:23:21 +0530

    Update Imdbposter.py

--- database/Imdbposter.py
+++ database/Imdbposter.py
@@ -93,7 +93,11 @@ async def get_movie_details(query, id=False, file=None):
         if plot and len(plot) > 800:
             plot = plot[:800] + "..."
 
-        poster_url = movie.get('full-size cover url')
+        poster_url = (
+    movie.get('cover url') or 
+    movie.get('full-size cover url') or 
+    movie.get('thumbnail url')
+        )
 
         return {
             'title': movie.get('title'),
